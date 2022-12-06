998523f591db4ba3e84c8015a4f0d14885dcf063
divo <divoapple@gmail.com>
2022-12-06 14:36:26 +0000

    Fix sketch in node
    This is your sign to dry

--- middleware/sketch.js
+++ middleware/sketch.js
@@ -3,24 +3,33 @@ import render_page from '../lib/render_setup.js';
 const sketch = ({width, height, canvas, data}) => {
   return ({ context, width, height, data, canvas }) => {
     const safe_area = 15; // mm!
-
-    let scale;
-    let y = 0;
-    let x = 0;
-
     const img = data['img'];
+
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
+    let scale;
+    let ratio;
+    let y = 0;
+    let x = 0;
+    let s_width;
+    let s_height;
+
     if (is_landscape(img)) {
       scale = width / img.width;
+      ratio = img.width / img.height;
       y = (height - (img.height * scale)) / 2;
+      s_width = (img.width * scale) - (safe_area * 2)
+      s_height = s_width / ratio;
     } else {
       scale = height / img.height;
+      ratio = img.height / img.width;
       x = (width - (img.width * scale)) / 2;
+      s_height = (img.height * scale) - (safe_area * 2)
+      s_width = s_height / ratio;
     }
 
-    context.drawImage(img, safe_area + x, safe_area + y, (img.width * scale) - (safe_area * 2), (img.height * scale) - (safe_area * 2));
+    context.drawImage(img, safe_area + x, safe_area + y, s_width, s_height);
   };
 };
 
