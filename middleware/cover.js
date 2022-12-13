e788fd9fe27de1561a0333f89bca3653961d4814
divo <divoapple@gmail.com>
2022-12-13 12:35:13 +0000

    POC sharing JS between rails and node
    Node is using a package
    Rails importmap seems unable to use a local package, so hack it in
    using a sym link

--- middleware/cover.js
+++ middleware/cover.js
@@ -1,34 +1,5 @@
 import render_page from '../lib/render_setup.js';
-
-const sketch = ({width, height, canvas, data}) => {
-  return ({ context, width, height, data, canvas }) => {
-    let scale;
-    let y = 0;
-    let x = 0;
-
-    const img = data['img'];
-    const name = data['name'];
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
-
-    if (is_landscape(img)) {
-      scale = width / img.width;
-    } else {
-      scale = height / img.height; // TODO: How to layout landscape covers? Easiest way is to simply not allow it
-    }
-
-    const s_height = img.height * scale;
-
-    context.drawImage(img, x, y, (img.width * scale), s_height);
-
-    const fontSize = 6; //TODO: What is going on with the scaling here? What is canvas sketch doing? It's all in mm??
-    context.fillStyle = 'black';
-    context.textAlign = 'center';
-    context.textBaseline = 'middle';
-    context.font = `normal ${fontSize}pt Helvetica`;
-    context.fillText(name, width / 2, s_height + (( height - s_height) / 2), width);
- };
-};
+import { cover_sketch } from 'sketches'
 
 const is_landscape = (image) => {
   return image.width > image.height;
@@ -41,7 +12,7 @@ const render_cover = async (req, res, next) => {
   // This works because the cover is already part of the album
   // If it becomes a distrinct image, update the fetcher to download it
   const page = req.body.cover;
-  render_promises.push(render_page(sketch, page, job_id, 'cover'));
+  render_promises.push(render_page(cover_sketch, page, job_id, 'cover'));
 
   Promise.all(render_promises).then(() => {
     console.log('[' + job_id + ']' + ' Cover rendering complete');
