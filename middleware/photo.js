8512e88a3fce563f065dc48fa6ab13a46f23f197
divo <divoapple@gmail.com>
2022-12-13 14:17:22 +0000

    Render sections in pdf

--- middleware/photo.js
+++ middleware/photo.js
@@ -1,13 +1,19 @@
 import render_page from '../lib/render_setup.js';
 import { createCanvas } from 'canvas';
-import { photo_sketch } from 'sketches';
+import { photo_sketch, section_sketch } from 'sketches';
 
 const photos = async (req, res, next) => {
   const render_promises = [];
   const job_id = req.body.job_id;
 
   req.body.pages.forEach(async (page) => {
-    render_promises.push(render_page(photo_sketch, page, job_id));
+    if (page.page_class == 'section-page') {
+      render_promises.push(render_page(section_sketch, page, job_id));
+    } else if (page.page_class = 'photo-content') {
+      render_promises.push(render_page(photo_sketch, page, job_id));
+    } else {
+      console.log("Unknown page type, skipping rendering!\n" + page)
+    }
   });
 
   Promise.all(render_promises).then(() => {
