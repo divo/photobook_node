f50986518007b9cff65a0975de1fd33f6d2d8ed7
divo <divoapple@gmail.com>
2023-01-04 15:14:26 +0000

    Add logic to position section-pages on odd number page, with blank page on opposite size

--- middleware/pdf.js
+++ middleware/pdf.js
@@ -5,34 +5,42 @@ import child_process from 'child_process';
 const merge_pdf = async (req, res, next) => {
   const pdfsToMerge = [];
   const mergedPdf = await PDFDocument.create();
-
   const job_id = req.body.job_id;
 
   // Add the cover
   const coverBuffer = fs.readFileSync('./tmp/output/' + job_id + '/cover.pdf');
   pdfsToMerge.push(coverBuffer);
 
-  const insideCoverBuffer = fs.readFileSync('./tmp/output/' + job_id + '/inside_cover.pdf');
+  const blankPageBuffer = fs.readFileSync('./tmp/output/' + job_id + '/inside_cover.pdf');
   if (req.body.magazine) { // Requirement from Printers
-    pdfsToMerge.push(insideCoverBuffer);
+    pdfsToMerge.push(blankPageBuffer);
   }
 
   // Add all the pages
   // TODO: Just use the directory contents? Will probably mess up the ordering
   req.body.pages.forEach(async (page) => {
+    // Section pages need to be on an odd page number with a blank left hand page 
+    if (page.page_class == 'section-page') {
+      if (!(pdfsToMerge.length % 2)) {
+        pdfsToMerge.push(blankPageBuffer);
+      }
+      pdfsToMerge.push(blankPageBuffer);
+    }
+
     const key = page.key;
     const pdfBuffer = fs.readFileSync('./tmp/output/' + job_id + '/' + key + '.pdf');
     pdfsToMerge.push(pdfBuffer);
   });
 
   // Use the blank inside at the end of the pdf
-  // If even no. of pages add two blanks
-  // if odd no of pages add one blank
+  // If even no. of pages add three blanks
+  // if odd no of pages add two blank
   // This requirement is from the Printer
   if (pdfsToMerge.length % 2) {
-    pdfsToMerge.push(insideCoverBuffer);
+    pdfsToMerge.push(blankPageBuffer);
   }
-  pdfsToMerge.push(insideCoverBuffer);
+  pdfsToMerge.push(blankPageBuffer);
+  pdfsToMerge.push(blankPageBuffer);
 
   for (const pdfBytes of pdfsToMerge) {
     const pdf = await PDFDocument.load(pdfBytes);
