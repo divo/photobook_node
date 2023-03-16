9fb1a67df71cafa9cb1c54f9adf4a92d267fa2b4
divo <divoapple@gmail.com>
2023-03-16 13:17:24 +0000

    Batch up photo processing to cut down on memory use

--- middleware/photo.js
+++ middleware/photo.js
@@ -1,26 +1,37 @@
 import render_page from '../lib/render_setup.js';
 import { createCanvas } from 'canvas';
 import { photo_sketch, section_sketch } from '@divo/photobook-sketches';
+import { collect_promises, await_promises } from '../lib/promise_batching.js';
+import { promisify } from "util";
+
+var job_id = null;
+var size = null; // ugh
+const slice_size = 10;
 
 const photos = async (req, res, next) => {
-  const render_promises = [];
-  const job_id = req.body.job_id;
-  const size = req.body.size;
+  job_id = req.body.job_id;
+  size = req.body.size;
 
-  req.body.pages.forEach(async (page) => {
-    if (page.page_class == 'section-page') {
-      render_promises.push(render_page(section_sketch, page, job_id, size));
-    } else if (page.page_class = 'photo-content') {
-      render_promises.push(render_page(photo_sketch, page, job_id, size));
-    } else {
-      console.log("Unknown page type, skipping rendering!\n" + page)
-    }
-  });
+  const pages = req.body.pages;
 
-  Promise.all(render_promises).then(() => {
+  const promises = collect_promises(pages, 0, slice_size, render);
+  await_promises(promises, pages, 0, slice_size, render, function() {
     console.log('[' + job_id + ']' + ' Rendering complete');
+    size = null
+    job_id = null
     next();
   });
+}
+
+const render = function(page) {
+  if (page.page_class == 'section-page') {
+    return render_page(section_sketch, page, job_id, size)
+  } else if (page.page_class = 'photo-content') {
+    return render_page(photo_sketch, page, job_id, size)
+  } else {
+    console.log("Unknown page type, skipping rendering!\n" + page)
+    return null;
+  }
 }
 
 export default photos;
