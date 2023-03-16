9e88635569b0eb9cbc75dd9a918c421d955f0be5
divo <divoapple@gmail.com>
2023-03-16 10:22:38 +0000

    Make awful promise batching logic re-usable

--- middleware/fetch_images.js
+++ middleware/fetch_images.js
@@ -3,56 +3,26 @@ import { got } from 'got';
 import stream from 'stream';
 import extension from '../lib/file_util.js'
 import { promisify } from "util";
-
-Array.prototype.each_slice = function (size, callback){
-  for (var i = 0, l = this.length; i < l; i += size){
-    callback.call(this, this.slice(i, i + size));
-  }
-};
+import { collect_promises, await_promises } from '../lib/promise_batching.js';
 
 const slice_size = 15;
+var job_id = null;
 
 // The idea is for this thing to kick off a load of fetch jobs
-// TODO: Handle failre and retry
+// TODO: Handle failure and retry
 const fetch_images = async(req, res, next) => {
-  const job_id = req.body.job_id;
+  job_id = req.body.job_id;
   const pages = req.body.pages;
-  const no_pages = pages.length;
 
-  const fetch_promises = collect_promises(pages, job_id, 0, no_pages);
-  await_promises(fetch_promises, pages, job_id, 0, no_pages, function() {
-    next();
-  });
-};
-
-const collect_promises = function(pages, job_id, offset, size) {
-  return fetch_slice(job_id, pages.slice(offset, offset + slice_size))
+  const fetch_promises = collect_promises(pages, 0, slice_size, fetch_image);
+  await_promises(fetch_promises, pages, 0, slice_size, fetch_image, function() {
+    console.log('[' + job_id + ']' + ' All images downloaded')
+    job_id = null; // Not sure if this is necessary
+    next()
+  })
 }
 
-// This is pretty fucking horrible, the worst code I've written in a long time
-const await_promises = function(promises, pages, job_id, offset, size, succ_callback) {
-  Promise.all(promises).then(() => {
-    if (offset < size) {
-      console.log('More to fetch');
-      const promises = collect_promises(pages, job_id, offset, size);
-      await_promises(promises, pages, job_id, offset + slice_size, size, succ_callback);
-    } else {
-      console.log('[' + job_id + ']' + ' All images downloaded');
-      succ_callback();
-    }
-  });
-};
-
-const fetch_slice = function(job_id, slice) {
-  const fetch_promises = [];
-  slice.forEach(function(page) {
-    fetch_promises.push(fetch_image(page, job_id));
-  });
-
-  return fetch_promises;
-};
-
-const fetch_image = function(page, job_id) {
+const fetch_image = function(page) {
   const pipeline = promisify(stream.pipeline);
   const image_url = page.image_url;
   const key = page.key;
