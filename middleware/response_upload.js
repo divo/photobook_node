f3afd9df826f6d990450d1d3bcb3ca1b949c267b
divo <divoapple@gmail.com>
2023-03-21 12:00:54 +0000

    return pdf in response

---
+++ middleware/response_upload.js
@@ -0,0 +1,23 @@
+const response_upload = (req, res) => {
+  const job_id = req.body.job_id;
+  const album_id = req.body.photo_album;
+
+  const opts = {
+    root: global.__basedir + '/tmp/pdf/' + job_id
+  };
+
+  const file_name = album_id + '_' + job_id + '.pdf_scaled.pdf';
+  res.sendFile(file_name, opts, (err) => {
+    if (err) {
+      console.log(err);
+      res.status(err.status);
+      next(err);
+    }
+    else {
+      console.log('Sent:', file_name);
+      next();
+    }
+  });
+}
+
+export default response_upload;
