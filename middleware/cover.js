a12f7f627662c60d1d1ff335126d3e831f1c0e0a
divo <divoapple@gmail.com>
2023-03-23 10:20:15 +0000

    Compute and pass width_offset for cover

--- middleware/cover.js
+++ middleware/cover.js
@@ -10,15 +10,22 @@ const render_cover = async (req, res, next) => {
   const job_id = req.body.job_id;
   const size = req.body.size;
 
+  const no_pages = req.body.pages.length;
+  const width = (size[0] * 2) + spine(no_pages);
+
   // This works because the cover is already part of the album
   // If it becomes a distrinct image, update the fetcher to download it
   const page = req.body.cover;
-  render_promises.push(render_page(cover_sketch, page, job_id, size, 'cover'));
+  render_promises.push(render_page(cover_sketch, page, job_id, [width, size[1]], 'cover', size[0] + spine(no_pages)));
 
   Promise.all(render_promises).then(() => {
     console.log('[' + job_id + ']' + ' Cover rendering complete');
     next();
   });
+}
+
+const spine = (count) => {
+  return count / 17.49781277 // constant from the printers
 }
 
 export default render_cover;
